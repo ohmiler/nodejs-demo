e5365c5c76f4ac717ff15223ca79493509cce4fa
Patiphan Phengpao <misterpatipan123@gmail.com>
2022-09-09 17:52:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 let express = require("express");
 let app = express();
 
+app.get("/", (req, res, next) => {
+    res.json({
+        message: "Welcome to nodejs demo"
+    });
+});
+
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
